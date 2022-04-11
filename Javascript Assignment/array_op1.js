9cdc05c61e426bdc26e321e329e68b42825bdec2
pratikshakheme <pratikshakheme@gmail.com>
2022-04-11 14:28:40 +0530

    First Commit

---
+++ Javascript Assignment/array_op1.js
@@ -0,0 +1,22 @@
+
+let cities=['latur','solapur','beed'];
+console.log(cities);
+
+console.log(cities.length);
+
+cities.push('Nashik');
+cities.push('Pune');
+
+console.log(cities.length);
+
+cities[1]="Delhi";
+cities[2]='Chennai';
+cities.push('Kolkatta');
+
+console.log(cities);
+
+cities.pop();
+cities.pop();
+console.log(cities);
+console.log(cities.length);
+
---
+++ Javascript Assignment/var_demo.js
@@ -0,0 +1,25 @@
+
+//-------------Variable----------------//
+
+var age=34;
+console.log("age: "+age);
+{
+    var age=56;
+}
+console.log("age: After block "+age);
+
+//-------------Let ariable----------------//
+
+let marks1 = 78;
+console.log("marks1: "+marks1);
+{
+    let marks1=78;
+    console.log("marks inside block of let" +marks1);
+}
+console.log("marks after block" +marks1);
+
+
+//-------------Let ariable----------------//
+
+
+
